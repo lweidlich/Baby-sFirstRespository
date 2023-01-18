11629d6510143e3d324c675db7566d2c59f10880
theCoderSchool-CoralSprings <115885261+coralsprings-tcs@users.noreply.github.com>
2023-01-18 17:59:45 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame. locals import*
+import sys
 
 pygame.init()
 
@@ -8,8 +9,18 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 640, 480
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 running = 1
+colors = {
+    "fire": pygame.color('chartreuse4'),
+    "white": (255,255,255)
+}
 letters_guessed_wrong = []
 letters_guessed = []
+input_text = ""
+active = False
+
+
+
+input_rect = pygame.Rect(200, 200, 140, 32)
 
 def to_list(str:str):
     return list(str)
@@ -21,7 +32,25 @@ while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = 0
-        
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if input_rect.collidepoint(event.pos):
+                active = True
+            else:
+                active = False
+
+            if event.type == pygame.KEYDOWN:
+
+                #checking for backspace
+                if event.key == pygame.K_BACKSPACE:
+                    input_text = input_text[:-1]
+
+                else:                # if event.unicode.isalpha():
+                    input_text += event.unicode
+
+
+    print(colors["fire"])
+    print(input_text)
 
 screen.fill((120, 120, 120))
 pygame.display.flip()
