eccff6c21855f6f83b1a692a7408b6ff6b7a88c2
lweidlich <lindsayweidlich@gmail.com>
2023-04-26 18:01:17 -0400

    continue o function

--- tictactoe.py
+++ tictactoe.py
@@ -8,6 +8,10 @@ pygame.init()
 surface = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 x_turn, o_turn = False, False
+winner = {
+    "x": False,
+    "o": False
+}
 
 x_placed = {
     "tl": False,
@@ -34,80 +38,94 @@ o_placed = {
 }
 
 def bot():
+    global o_turn
     randnum = random.randint(1,9)
 
-    match randnum:
-        case 1: 
-            if x_placed["tl"] == False:
-                draw_o(150, 150)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+    try:
+        match randnum:
+            case 1: 
+                if x_placed["tl"] == False and o_turn == True:
+                    draw_o(150, 150)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
 
-        case 2: 
-            if x_placed["tc"] == False:
-                draw_o(450, 150)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+            case 2: 
+                if x_placed["tc"] == False and o_turn == True:
+                    draw_o(450, 150)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
 
-        case 3: 
-            if x_placed["tr"] == False:
-                draw_o(750, 150)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+            case 3: 
+                if x_placed["tr"] == False and o_turn == True:
+                    draw_o(750, 150)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
 
-        case 4: 
-            if x_placed["cl"] == False:
-                draw_o(150, 450)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+            case 4: 
+                if x_placed["cl"] == False and o_turn == True:
+                    draw_o(150, 450)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
 
-        case 5: 
-            if x_placed["cc"] == False:
-                draw_o(450, 450)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
-    
-        case 6: 
-            if x_placed["cr"] == False:
-                draw_o(750, 450)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+            case 5: 
+                if x_placed["cc"] == False and o_turn == True:
+                    draw_o(450, 450)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
+        
+            case 6: 
+                if x_placed["cr"] == False and o_turn == True:
+                    draw_o(750, 450)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
 
-        case 7: 
-            if x_placed["bl"] == False:
-                draw_o(150, 750)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+            case 7: 
+                if x_placed["bl"] == False and o_turn == True:
+                    draw_o(150, 750)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
 
-        case 8: 
-            if x_placed["bc"] == False:
-                draw_o(450, 750)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+            case 8: 
+                if x_placed["bc"] == False and o_turn == True:
+                    draw_o(450, 750)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
 
-        case 9: 
-            if x_placed["br"] == False:
-                draw_o(750, 750)
-            else:
-                bot()
-                o_turn = False
-                x_turn = True
+            case 9: 
+                if x_placed["br"] == False and o_turn == True:
+                    draw_o(750, 750)
+                    o_turn = False
+                    x_turn = True
+                    o_placed = True
+                else:
+                    return
+
+    except Exception as e:
+        print(str(e))
 
 
 def map():
@@ -130,57 +148,73 @@ def draw_x(
     pygame.draw.line(surface, (255,255,255), (start_x_2, start_y_2), (end_x_2, end_y_2))
 
 def draw_o(x,y):
-    pygame.draw.circle(surface, (255,255,255), (x,y), 5, width=2)
+    pygame.draw.circle(surface, (255,255,255), (x,y), 140, width=2)
 
 def win_check():
     if x_placed['tl'] and x_placed['tc'] and x_placed['tr'] == True:
         pygame.draw.line(surface, (255,255,255), (0,150), (900,150), width=4)
+        winner["x"] == True
 
     if x_placed['cl'] and x_placed['cc'] and x_placed['cr'] == True:
         pygame.draw.line(surface, (255,255,255), (0,450), (900,450), width=4)
+        winner["x"] == True
 
     if x_placed['bl'] and x_placed['bc'] and x_placed['br'] == True:
         pygame.draw.line(surface, (255,255,255), (0,750), (900,750), width=4)
+        winner["x"] == True
 
     if x_placed['tl'] and x_placed['cl'] and x_placed['bl'] == True:
         pygame.draw.line(surface, (255,255,255), (150,0), (150,900), width=4)
+        winner["x"] == True
 
     if x_placed['tc'] and x_placed['cc'] and x_placed['bc'] == True:
         pygame.draw.line(surface, (255,255,255), (450,0), (450,900), width=4)
+        winner["x"] == True
 
     if x_placed['tr'] and x_placed['cr'] and x_placed['br'] == True:
         pygame.draw.line(surface, (255,255,255), (750,0), (750,900), width=4)
+        winner["x"] == True
 
     if x_placed['tl'] and x_placed['cc'] and x_placed['br'] == True:
         pygame.draw.line(surface, (255,255,255), (0,0), (900,900), width=4)
+        winner["x"] == True
 
     if x_placed['tr'] and x_placed['cc'] and x_placed['bl'] == True:
         pygame.draw.line(surface, (255,255,255), (900,900), (0,0), width=4)
+        winner["x"] == True
 
 def win_checko():
     if o_placed['tl'] and o_placed['tc'] and o_placed['tr'] == True:
         pygame.draw.line(surface, (255,255,255), (0,150), (900,150), width=4)
+        winner["o"] == True
 
     if o_placed['cl'] and o_placed['cc'] and o_placed['cr'] == True:
         pygame.draw.line(surface, (255,255,255), (0,450), (900,450), width=4)
+        winner["o"] == True
 
     if o_placed['bl'] and o_placed['bc'] and o_placed['br'] == True:
         pygame.draw.line(surface, (255,255,255), (0,750), (900,750), width=4)
+        winner["o"] == True
 
     if o_placed['tl'] and o_placed['cl'] and o_placed['bl'] == True:
         pygame.draw.line(surface, (255,255,255), (150,0), (150,900), width=4)
+        winner["o"] == True
 
     if o_placed['tc'] and o_placed['cc'] and o_placed['bc'] == True:
         pygame.draw.line(surface, (255,255,255), (450,0), (450,900), width=4)
+        winner["o"] == True
 
     if o_placed['tr'] and o_placed['cr'] and o_placed['br'] == True:
         pygame.draw.line(surface, (255,255,255), (750,0), (750,900), width=4)
+        winner["o"] == True
 
     if o_placed['tl'] and o_placed['cc'] and o_placed['br'] == True:
         pygame.draw.line(surface, (255,255,255), (0,0), (900,900), width=4)
+        winner["o"] == True
 
     if o_placed['tr'] and o_placed['cc'] and o_placed['bl'] == True:
         pygame.draw.line(surface, (255,255,255), (900,900), (0,0), width=4)
+        winner["o"] == True
 
 
 def pos_check(x,y):
@@ -247,7 +281,7 @@ def pos_check(x,y):
         x_turn = False
         o_turn = True
 
-def pos_checko(x,y):
+def pos_checko(x,y): #TODO: delete
     #tl corner
     if x <= 300 and y <= 300:
         draw_o(150, 150)
@@ -312,7 +346,7 @@ def pos_checko(x,y):
         x_turn = True
 
 def main():
-    while True:
+    while winner["x"] != True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -330,9 +364,10 @@ def main():
                 else: 
                     pass
 
+            map()
             win_check()
             win_checko()
-            map()
+            bot()
 
 
             pygame.display.update()
